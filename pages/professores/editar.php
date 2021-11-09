46d92985162d4d3bca3be7c8a4a6dc83947addb1
Jefferson Clark <jeffersonclarkti@gmail.com>
2021-11-09 17:00:52 -0300

    CEP

--- pages/professores/editar.php
+++ pages/professores/editar.php
@@ -63,7 +63,7 @@
               <div class="row">
                 <div class="col-lg-12">
                   <div class="form-group">
-                    <input name="nome" id="nome" type="text" class="form-control" placeholder="Nome completo">
+                    <input name="nome" id="nome" type="text" class="form-control" placeholder="Nome completo" required>
 										<input name="id" id="id" type="hidden">
                   </div>
                 </div>
@@ -71,60 +71,65 @@
               <div class="row">
                 <div class="col-lg-12">
                   <div class="form-group">
-                    <input name="email" id="email" type="text" class="form-control" placeholder="E-mail">
+                    <input name="email" id="email" type="text" class="form-control" placeholder="E-mail" required>
                   </div>
                 </div>
               </div>
               <div class="row">
                 <div class="col-lg-6">
                   <div class="form-group">
-                    <input name="cpf" id="cpf" type="text" class="form-control" placeholder="CPF">
+                    <input name="cpf" id="cpf" type="text" class="form-control" placeholder="CPF" required>
                   </div>
                 </div>
                 <div class="col-lg-6">
                   <div class="form-group">
-                    <input name="rg" id="rg" type="text" class="form-control" placeholder="RG">
+                    <input name="rg" id="rg" type="text" class="form-control" placeholder="RG" required>
                   </div>
                 </div>
               </div>
               <div class="row">
                 <div class="col-lg-4">
                   <div class="form-group">
-                    <input name="cep" id="cep" type="text" class="form-control" placeholder="CEP">
+                    <input name="cep" id="cep" type="text" class="form-control" placeholder="CEP" required>
                   </div>
                 </div>
                 <div class="col-lg-4">
                   <div class="form-group">
-                    <input name="cidade" id="cidade" type="text" class="form-control" placeholder="Cidade">
+                    <input name="cidadeFake" id="cidadeFake" type="text" class="form-control" placeholder="Cidade" disabled required>
+                    <input name="cidade" id="cidade" type="hidden">
                   </div>
                 </div>
                 <div class="col-lg-4">
                   <div class="form-group">
-                    <input name="uf" id="uf" type="text" class="form-control" placeholder="UF">
+                    <input name="ufFake" id="ufFake" type="text" class="form-control" placeholder="UF" disabled required>
+                    <input name="uf" id="uf" type="hidden">
                   </div>
                 </div>
               </div>
               <div class="row">
                 <div class="col-lg-12">
                   <div class="form-group">
-                    <input name="rua" id="rua" type="text" class="form-control" placeholder="Endereço">
+                    <input name="ruaFake" id="ruaFake" type="text" class="form-control" placeholder="Endereço" disabled required>
+                    <input name="rua" id="rua" type="hidden">
                   </div>
                 </div>
               </div>
               <div class="row">
                 <div class="col-lg-4">
                   <div class="form-group">
-                    <input name="numero" id="numero" type="text" class="form-control" placeholder="Número">
+                    <input name="numero" id="numero" type="text" class="form-control" placeholder="Número" required>
                   </div>
                 </div>
                 <div class="col-lg-4">
                   <div class="form-group">
-                    <input name="bairro" id="bairro" type="text" class="form-control" placeholder="Bairro">
+                    <input name="bairroFake" id="bairroFake" type="text" class="form-control" placeholder="Bairro" disabled required>
+                    <input name="bairro" id="bairro" type="hidden">
                   </div>
                 </div>
                 <div class="col-lg-4">
                   <div class="form-group">
-                    <input name="complemento" id="complemento" type="text" class="form-control" placeholder="Complemento">
+                    <input name="complementoFake" id="complementoFake" type="text" class="form-control" placeholder="Complemento" disabled required>
+                    <input name="complemento" id="complemento" type="hidden">
                   </div>
                 </div>
               </div>
@@ -158,107 +163,125 @@
 		</div>
 	</div>
 </footer>
+  
+<!-- Main jQuery -->
+<script src="../../plugins/jquery/jquery.js"></script>
+<!-- Bootstrap 4.3.2 -->
+<script src="../../plugins/bootstrap/js/popper.js"></script>
+<script src="../../plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="../../plugins/counterup/jquery.easing.js"></script>
+<!-- Slick Slider -->
+<script src="../../plugins/slick-carousel/slick/slick.min.js"></script>
+<!-- Counterup -->
+<script src="../../plugins/counterup/jquery.waypoints.min.js"></script>
 
-   
+<script src="../../plugins/shuffle/shuffle.min.js"></script>
+<script src="../../plugins/counterup/jquery.counterup.min.js"></script>
 
-    <!-- 
-    Essential Scripts
-    =====================================-->
+<script src="../../js/script.js"></script>
+<script src="../../js/contact.js"></script>
 
-    
-    <!-- Main jQuery -->
-    <script src="../../plugins/jquery/jquery.js"></script>
-    <!-- Bootstrap 4.3.2 -->
-    <script src="../../plugins/bootstrap/js/popper.js"></script>
-    <script src="../../plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../../plugins/counterup/jquery.easing.js"></script>
-    <!-- Slick Slider -->
-    <script src="../../plugins/slick-carousel/slick/slick.min.js"></script>
-    <!-- Counterup -->
-    <script src="../../plugins/counterup/jquery.waypoints.min.js"></script>
-    
-    <script src="../../plugins/shuffle/shuffle.min.js"></script>
-    <script src="../../plugins/counterup/jquery.counterup.min.js"></script>
-    
-    <script src="../../js/script.js"></script>
-    <script src="../../js/contact.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
-		<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script>
 
-		<script>
+  loader();
 
-			loader();
+  function loader(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const param = urlParams.get('id')
+    $("#id").val(param);
+    const config = {
+      method: 'get',
+      url: 'https://sportsfree-dev.herokuapp.com/professor/'+param,
+      headers: { }
+    };
+    axios(config)
+    .then(function (response) {
+      $("#nome").val(response.data.nome);
+      $("#email").val(response.data.email);
+      $("#cep").val(response.data.endereco.cep);
+      $("#rg").val(response.data.rg);
+      $("#cpf").val(response.data.cpf);
+      $("#uf").val(response.data.endereco.uf);
+      $("#rua").val(response.data.endereco.rua);
+      $("#numero").val(response.data.endereco.numero);
+      $("#cidade").val(response.data.endereco.cidade);
+      $("#bairro").val(response.data.endereco.bairro);
+      $("#complemento").val(response.data.endereco.complemento);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
 
-			function loader(){
-				const queryString = window.location.search;
-				const urlParams = new URLSearchParams(queryString);
-				const param = urlParams.get('id')
-				$("#id").val(param);
-				const config = {
-          method: 'get',
-          url: 'https://sportsfree-dev.herokuapp.com/professor/'+param,
-          headers: { }
-        };
-        axios(config)
-        .then(function (response) {
-					$("#nome").val(response.data.nome);
-					$("#email").val(response.data.email);
-					$("#cep").val(response.data.endereco.cep);
-					$("#rg").val(response.data.rg);
-					$("#cpf").val(response.data.cpf);
-					$("#uf").val(response.data.endereco.uf);
-					$("#rua").val(response.data.endereco.rua);
-					$("#numero").val(response.data.endereco.numero);
-					$("#cidade").val(response.data.endereco.cidade);
-					$("#bairro").val(response.data.endereco.bairro);
-					$("#complemento").val(response.data.endereco.complemento);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-			}
+  $("#update").submit(function( event ) {
+    event.preventDefault();
+    const data = {
+      "id": $("#id").val(),
+      "nome": $("#nome").val(),
+      "email": $("#email").val(),
+      "cpf": $("#cpf").val(),
+      "rg": $("#rg").val(),
+      "endereco": {
+          "cep": $("#cep").val(),
+          "cidade": $("#cidade").val(),
+          "uf": $("#uf").val(),
+          "rua": $("#rua").val(),
+          "numero": $("#numero").val(),
+          "bairro": $("#bairro").val(),
+          "complemento": $("#complemento").val()
+      }
+    }
 
-			$( "#update" ).submit(function( event ) {
+    const config = {
+      method: 'put',
+      url: 'https://sportsfree-dev.herokuapp.com/professor',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
 
-        event.preventDefault();
-				const data = {
-					"id": $("#id").val(),
-					"nome": $("#nome").val(),
-					"email": $("#email").val(),
-					"cpf": $("#cpf").val(),
-					"rg": $("#rg").val(),
-					"endereco": {
-							"cep": $("#cep").val(),
-							"cidade": $("#cidade").val(),
-							"uf": $("#uf").val(),
-							"rua": $("#rua").val(),
-							"numero": $("#numero").val(),
-							"bairro": $("#bairro").val(),
-							"complemento": $("#complemento").val()
-					}
-				}
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
-        const config = {
-          method: 'put',
-					url: 'https://sportsfree-dev.herokuapp.com/professor',
-					headers: { 
-						'Content-Type': 'application/json'
-					},
-					data : data
-        };
+  });
 
-        axios(config)
-        .then(function (response) {
-          console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-
+  $("#cep").keyup(function(){
+    if($("#cep").val().length === 9){
+      const config = {
+        method: 'get',
+        url: `https://viacep.com.br/ws/${$("#cep").val()}/json/`
+      };
+      axios(config)
+      .then(function (response) {
+        $("#cidade").val(response.data.localidade);
+        $("#uf").val(response.data.uf);
+        $("#rua").val(response.data.logradouro);
+        $("#bairro").val(response.data.bairro);
+        $("#complemento").val(response.data.complemento);
+        $("#cidadeFake").val(response.data.localidade);
+        $("#ufFake").val(response.data.uf);
+        $("#ruaFake").val(response.data.logradouro);
+        $("#bairroFake").val(response.data.bairro);
+        $("#complementoFake").val(response.data.complemento);
+        $("#numero").focus();
+      })
+      .catch(function (error) {
+        console.log(error);
       });
+    }
+  });
 
-		</script>
+</script>
 
-  </body>
-  </html>
+</body>
+</html>
    
