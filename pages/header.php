7baa3027be616d9e1e28f0802efe5d5c7a2284ba
Jefferson Clark <jeffersonclarkti@gmail.com>
2021-12-14 11:02:42 -0300

    Ajuste

--- pages/header.php
+++ pages/header.php
@@ -14,6 +14,7 @@
         <li class="nav-item"><a class="nav-link" href="/pages/professores/">Professores</a></li>
         <li class="nav-item"><a class="nav-link" href="/pages/doadores/">Doadores</a></li>
 				<li class="nav-item"><a class="nav-link" href="/pages/esportes/">Esportes</a></li>
+				<li class="nav-item"><a class="nav-link btn btn-main btn-round-full text-white" href="/">Logoff</a></li>
 			</ul>
 		  </div>
 		</div>
